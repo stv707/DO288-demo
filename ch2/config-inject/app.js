22afe57642d1641f16db35c85920fa64fc367ccf
Steven <steven@parrotpos.my>
2023-09-25 22:31:55 +0800

    1695681116

--- ch2/config-inject/app.js
+++ ch2/config-inject/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
     const VAR1 = process.env.VAR1 || 'VARIABLE MISSING';
