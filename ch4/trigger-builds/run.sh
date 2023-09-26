2db32dafbc9adb8e2f3b14a53d661152542448b0
Student User <student@workstation.lab.example.com>
2023-09-26 19:38:34 -0400

    notes v1

--- ch4/trigger-builds/run.sh
+++ ch4/trigger-builds/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
-URL="https://api.ocp4.example.com:6443/apis/build.openshift.io/v1/namespaces/bc-demo-project/buildconfigs/bcdemo/webhooks/vy8uX-9KX6oyEODKAoQl/github"
+URL="https://api.ocp4.example.com:6443/apis/build.openshift.io/v1/namespaces/bc-demo-project/buildconfigs/bcdemo/webhooks/FPrC_bf_Q7FOszS5rwqm/github"
+
 
 curl -X POST -k -H "X-GitHub-Event: push" -H "Content-Type: application/json" -d '{"ref":"refs/heads/main"}' ${URL} 
